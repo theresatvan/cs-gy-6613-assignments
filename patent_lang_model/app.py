5ef7621af1a858b20a10a22b30426250f756d804
unknown <theresatvan@gmail.com>
2023-04-07 17:33:36 -0400

    Add model selection feature

--- patent_lang_model/app.py
+++ patent_lang_model/app.py
@@ -1,26 +1,29 @@
 import streamlit as st
 from transformers import pipeline
+from huggingface_hub import HfApi, ModelFilter
 
+# Set up Hugging Face Hub API client
+api = HfApi()
+
+# Display title
 st.title("Text Sentiment Analyzer")
+
+# Retrieve all text classification models
+models = api.list_models(filter=ModelFilter(task="text-classification"))[:10]
+model_ids = [model.modelId for model in models]
 
 # Create submission form
 form = st.form("sentiment-form")
+select_model = form.selectbox("Select a pretrained model", model_ids)
 input = form.text_area('Enter your text here.')
 submit = form.form_submit_button("Submit")
 
 if submit:
-    # Use pre-trained sentiment analysis model
-    classifier = pipeline(task="sentiment-analysis")
+    # Create pipeline to user's selected pre-trained model
+    classifier = pipeline(task="sentiment-analysis", model=select_model)
     
     # Extract prediction from the results
-    pred = classifier(input)[0]
-    label= pred['label']
-    score = pred['score']
+    pred = classifier(input)
     
-    if label == "POSITIVE":
-        # Green output text box
-        st.success("{} sentiment (score: {})".format(label, score))
-        
-    else:
-        # Red output text box
-        st.error("{} sentiment (score: {})".format(label, score))
+    # Display prediction
+    st.write(pred)
